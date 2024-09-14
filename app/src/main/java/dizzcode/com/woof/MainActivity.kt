a73fb7327fc55a0a1286ebb61f9a57cbdee31b2b
Shavinda Dissanayake <shavinda.diz@gmail.com>
2024-09-14 22:10:34 +0530

    Add colours

--- app/src/main/java/dizzcode/com/woof/MainActivity.kt
+++ app/src/main/java/dizzcode/com/woof/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,11 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WoofTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    WoofApp()
                 }
             }
         }
@@ -31,17 +29,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun WoofApp() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     WoofTheme {
-        Greeting("Android")
+        WoofApp()
     }
 }

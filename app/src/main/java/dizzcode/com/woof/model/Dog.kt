a25b0914307687894e00b1a8da38f499072eff7d
Shavinda Dissanayake <shavinda.diz@gmail.com>
2024-09-15 16:08:20 +0530

    Add app icons

---
+++ app/src/main/java/dizzcode/com/woof/model/Dog.kt
@@ -0,0 +1,11 @@
+package dizzcode.com.woof.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Dog(
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val name: Int,
+    val age: Int,
+    @StringRes val hobbies: Int
+)
